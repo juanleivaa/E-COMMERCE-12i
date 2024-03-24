2e826bafc934e79d2093af4c19330e2d266af378
Juan Ignacio Leiva <jleiva21082004@gmail.com>
2024-03-24 16:53:05 -0300

    Sección de Login

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import './CSS/LoginSignup.css'
 
 const LoginSignup = () => {
   return (
-    <div>
-      
+    <div className="loginsignup">
+        <div className="loginsignup-container">
+          <h1>Sign up</h1>
+          <div className="loginsignup-fields">
+            <input type="text" placeholder='Nombre'/>
+            <input type="email" placeholder='Su correo electronico'/>
+            <input type="password" placeholder='Contraseña'/>
+          </div>
+          <button>Continuar</button>
+          <p className='loginsignup-login'>Ya tienes una cuenta? <span>Login</span></p>
+          <div className="loginsignup-agree">
+            <input type="checkbox" name='' id='' />
+            <p>Para continuar, debe aceptar los terminos y condiciones </p>
+          </div>
+        </div>
+
     </div>
   )
 }
